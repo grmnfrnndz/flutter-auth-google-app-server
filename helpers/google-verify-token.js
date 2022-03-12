b1c495c8c8829a6cb78c0b25644d8b7fa41fb350
German Contreras <german.contreras@aspsa.cl>
2022-03-12 14:02:02 -0300

    cambios en los helpers

--- helpers/google-verify-token.js
+++ helpers/google-verify-token.js
@@ -18,7 +18,8 @@ const validarGoogleIdToken = async (token) => {
             ]  
         });
         const payload = ticket.getPayload();
-        // const userid = payload['sub'];
+        
+        console.log('===============payload============');
         console.log(payload);
     
         return {
